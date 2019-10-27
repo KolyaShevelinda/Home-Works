ad2dc4dc8222f36f7c59f6bc8243468a9ef91b08
KolyaShevelinda <kolyashevelinda@gmail.com>
2019-10-27 21:14:16 +0200

    the Task №4

--- homeWorks_Js/21-10-2019/js/index.js
+++ homeWorks_Js/21-10-2019/js/index.js
@@ -1,12 +1,14 @@
-'use strict';
-
-// Задание №1
+// 'use strict';
+// ****************************
+// the Task №1
 // var x = prompt('Введите Х', '1'), // не было запятой
 //     y = prompt('Введите Y', '1');
 
 // alert( 'Сумма: ' + x + y );
+// ****************************
 
-// Задание №2
+// ****************************
+// the Task №2
 // var x = 3,
 //     y = x * 3,
 //     z = x + y;
@@ -14,13 +16,51 @@
 //     alert(x);
 //     alert(y);
 //     alert(z);
+// ****************************
 
-// Задание №3
-var firstName = prompt('Введите Имя: ', ''),
-    lastName = prompt('Введите Фамилию: ', '');
+// ****************************
+// the Task №3
+// var firstName = prompt('Введите Имя: ', ''),
+//     lastName = prompt('Введите Фамилию: ', '');
 
-    firstName = firstName.charAt(0).toUpperCase() + firstName.slice(1);
-    lastName = lastName.charAt(0).toUpperCase() + lastName.slice(1);
+//     firstName = firstName.charAt(0).toUpperCase() + firstName.slice(1);
+//     lastName = lastName.charAt(0).toUpperCase() + lastName.slice(1);
 
-alert(`" What’s up ${``+ firstName} ${``+ lastName} "`);
+// alert(`" What’s up ${``+ firstName} ${``+ lastName} "`);
+// ****************************
 
+// ****************************
+// the Task №4
+// var x = prompt('Input x: ', ''),
+//     y = prompt('Input y: ', '');
+
+// summ = parseInt(x) + parseInt(y);
+// alert(`x * y =  ${x * y}`);
+// alert(`x / y =  ${x / y}`);
+// alert(`x - y =  ${x - y}`);
+// alert(`x + y =  ${summ}`);
+// ****************************
+
+// ****************************
+// the Task №5
+// ****************************
+
+// ****************************
+// the Task №6
+// ****************************
+
+// ****************************
+// the Task №7
+// ****************************
+
+// ****************************
+// the Task №8
+// ****************************
+
+// ****************************
+// the Task №9
+// ****************************
+
+// ****************************
+// the Task №10
+// ****************************
