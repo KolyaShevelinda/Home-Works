0096114a884026cf369f4bb441c06f59bcdd9e1d
KolyaShevelinda <kolyashevelinda@gmail.com>
2019-12-02 04:58:47 +0200

    not done task№2

--- homeWorks_Js/1-11-2019/js/index.js
+++ homeWorks_Js/1-11-2019/js/index.js
@@ -1,26 +1,71 @@
 // *********************
 //Task №1
 
-let findOption = prompt('Введите название свойства объекта (firstname, lastname, age) : ', '');
+// let findOption = prompt('Введите название свойства объекта (firstname, lastname, age) : ', '');
 
-let person = {
-    firstname: 'Nike',
-    lastname: 'Shevelinda',
-    age: 40
-}
+// let person = {
+//     firstname: 'Nike',
+//     lastname: 'Shevelinda',
+//     age: 40
+// }
 
-console.log(person[findOption]);
+// console.log(person[findOption]);
 
-if (typeof person[findOption] === 'undefined') {
-    let value = prompt('Введите значение свойства ' + findOption + ' объекта', '');
-    person[findOption] = value;
-    console.log(person);
-}
+// if (typeof person[findOption] === 'undefined') {
+//     let value = prompt('Введите значение свойства ' + findOption + ' объекта', '');
+//     person[findOption] = value;
+//     console.log(person);
+// }
 
 // *********************
 
 // *********************
 //Task №2
+debugger;
+let person = {
+    firstname: 'Nike',
+    lastname: 'Shevelinda',
+    age: 40
+}
+console.log(person);
+
+let nameNewObj = new Object();
+person.nameNewObj = prompt('Введите название объекта : ', '');
+console.log(person);
+
+if (typeof person.nameNewObj === 'undefined') {
+
+    while (true) {
+
+        let fildObj = prompt('Введите название поля объекта ' + person.nameNewObj, '');
+        person.nameNewObj = fildObj;
+
+        let valueNewObj = prompt('Введите значение поля ' + person.nameNewObj + ' объекта ', '');
+        fildObj = valueNewObj;
+
+        if (nameNewObj === null || fildObj === null || valueNewObj === null) {
+            alert('Окончание работы с объектом ');
+            break;
+        }
+        console.log(person);
+    }
+
+} 
+
+// if (typeof person.nameNewObj === 'undefined') {
+
+//     person.nameNewObj = prompt('Введите название объекта : ', '');
+
+//     let fildObj = prompt('Введите название свойства объекта ' + person.nameNewObj, '');
+//     person.nameNewObj = fildObj;
+
+//     // let valueNewObj = prompt('Введите название свойства объекта : ' + nameNewObj + ' объекта ', '');
+//     // nameNewObj.fildObj = valueNewObj;
+
+//     console.log(person);
+
+// }
+
 // *********************
 
 // *********************
